46837a6e9954f194cbb58220453f8d0cdb5736e1
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2022-05-26 21:10:44 +0200

    add ui page error 404

--- src/n1-main/m1-ui/error404/Error404.tsx
+++ src/n1-main/m1-ui/error404/Error404.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import style from './error404.module.css'
 
 const Error404 = () => {
     return (
         <div>
-            <h1>Error 404. Page not found!</h1>
+            <div className={style.wrapper}>
+                <div className={style.content}>
+                    <div className={style.firstSymbol}>4</div>
+                    <div className={style.secondSymbol}>0</div>
+                    <div className={style.thirdSymbol}>4</div>
+                </div>
+                <span className={style.description}> an error occurred, please try again later</span>
+            </div>
         </div>
     );
 };
