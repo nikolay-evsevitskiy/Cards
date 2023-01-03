c863b50a02498f9e44f074beff2cbc15a2369cd8
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2023-01-03 22:02:21 +0100

    build ui container of login

--- src/n2-features/f1-auth/a2-login/Login.tsx
+++ src/n2-features/f1-auth/a2-login/Login.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import loginStyles from './login.module.css';
 
 const Login = () => {
     return (
-        <div>
-            <h2>It-incubator</h2>
-            <h3>Sign In</h3>
-            <div className={'inputField'}>
+        <div className={loginStyles.mainContainer}>
+            <div className={loginStyles.mainBlock}>
+                <h1>Cards</h1>
+                <h2>Sign In</h2>
+                <div className={loginStyles.inputField}>
+                    <input type={'email'}/>
+                    <input type={'password'}/>
 
+                </div>
             </div>
         </div>
+
     );
 };
 
