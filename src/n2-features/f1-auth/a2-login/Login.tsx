26f26e3416cb031d8823a79847b73e40363a40db
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2023-01-04 22:08:32 +0100

    create styles in login component

--- src/n2-features/f1-auth/a2-login/Login.tsx
+++ src/n2-features/f1-auth/a2-login/Login.tsx
@@ -5,11 +5,20 @@ const Login = () => {
     return (
         <div className={loginStyles.mainContainer}>
             <div className={loginStyles.mainBlock}>
-                <h1>Cards</h1>
+                <h1>Cards app</h1>
                 <h2>Sign In</h2>
                 <div className={loginStyles.inputField}>
-                    <input type={'email'}/>
+                    <label htmlFor='txtEmail'>email</label>
+                    <input id='txtEmail' type='email'/>
                     <input type={'password'}/>
+                    <div>Forgot Password</div>
+                </div>
+                <div className={loginStyles.footerBlock}>
+                    <button>Login</button>
+                    <div className={loginStyles.signUpBlock}>
+                        <div>Don't have an account?</div>
+                        <button>Sign up</button>
+                    </div>
 
                 </div>
             </div>
