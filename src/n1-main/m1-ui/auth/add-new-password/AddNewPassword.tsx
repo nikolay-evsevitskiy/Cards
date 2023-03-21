899e61635705f5de64e95f41cee5d57d1361c71f
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2023-03-21 20:57:55 +0100

    add bll with AddNewPassword

--- src/n1-main/m1-ui/auth/add-new-password/AddNewPassword.tsx
+++ src/n1-main/m1-ui/auth/add-new-password/AddNewPassword.tsx
@@ -1,11 +1,15 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import style from "./addNewPassword.module.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEye, faEyeSlash} from "@fortawesome/free-solid-svg-icons";
 
 const AddNewPassword = () => {
+    const [input, setInput] = useState<string>('')
     const [stateEye, setStateEye] = useState<boolean>(false);
     const changeEyeHandle = () => setStateEye(!stateEye);
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setInput(e.currentTarget.value)
+    }
     return (
         <div className={style.mainContainer}>
             <div className={style.mainBlock}>
@@ -13,8 +17,13 @@ const AddNewPassword = () => {
                 <h2>Create new password</h2>
                 <form>
                     <div className={style.inputBlock}>
-                        <input className={style.inputField} type={stateEye ? 'password' : 'text'}
-                               required/>
+                        <input
+                            className={style.inputField}
+                            type={stateEye ? 'password' : 'text'}
+                            value={input}
+                            onChange={onChangeHandler}
+                            required
+                        />
                         <FontAwesomeIcon icon={stateEye ? faEye : faEyeSlash} inverse={true}
                                          onClick={changeEyeHandle} className={style.eyeIcon}/>
                         <label className={style.inputLabel}>Password</label>
--- src/n1-main/m3-api/pack-api.ts
+++ src/n1-main/m3-api/pack-api.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:
-    // 'https://neko-back.herokuapp.com/2.0',
-        process.env.REACT_APP_BACK_URL_CARDS
-        || 'http://localhost:7542/2.0/',
+    // baseURL:process.env.REACT_APP_BACK_URL_CARDS|| 'http://localhost:7542/2.0/',
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:7542/2.0/' : 'https://neko-back.herokuapp.com/2.0/',
     withCredentials: true,
 })
 
