f5b002a7bd0813abad72eb2f48675b9e82ed43b0
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2023-03-21 21:14:52 +0100

    changed base url

--- src/n1-main/m3-api/pack-api.ts
+++ src/n1-main/m3-api/pack-api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     // baseURL:process.env.REACT_APP_BACK_URL_CARDS|| 'http://localhost:7542/2.0/',
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:7542/2.0/' : 'https://neko-back.herokuapp.com/2.0/',
+    baseURL: 'https://neko-back.herokuapp.com/2.0/',
     withCredentials: true,
 })
 
